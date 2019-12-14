d1e9fc271967c4d639ec62bfeebecc07d14f2e31
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-12-14 14:58:37 +0530

    intialized

---
+++ src/components/Top.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import data from './data.json';
+
+export default class Top extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 'rajasthan',
+      filterdata: []
+    };
+    console.log(this.state.value);
+  }
+  handleChange = event => {
+    var a = event.target.value;
+    this.setState({ value: a }, () => console.log(this.state.value));
+  };
+  render() {
+    return (
+      <>
+        <div className="top">
+          <h1>Explore the life till the end</h1>
+          <div className="top-select">
+            <select onChange={this.handleChange}>
+              <option value="rajasthan">Rajasthan, Change place</option>
+              {['tamilnadu', 'kerala', 'himachalpradesh', 'rajasthan'].map(
+                place => (
+                  <option value={place}>{place}</option>
+                )
+              )}
+            </select>
+          </div>
+        </div>
+        <div className="card-grid">
+          <>
+            {data[this.state.value].map(item => (
+              <div className="card">
+                <img className="card-img" src={item.image} alt=""></img>
+                <p className="card-name">{item.name}</p>
+              </div>
+            ))}
+          </>
+        </div>
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+// var React = require("react");
 import React from 'react';
+
+// var ReactDom = require("react-dom");
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+import Top from './components/Top';
+// importing stylesheet
+import '../src/assets/stylesheet/style.scss';
+// header component
+function App() {
+  return <Top />;
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
